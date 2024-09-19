8bb958025a43ec5874ecb8419f2520384fc5cfef
eunryong.hwang <eunryong.hwang@gmail.com>
2024-09-20 02:21:44 +0900

    fix something

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import * as Minio from 'minio'
-const core = require('@actions/core');
-const fs = require('fs');
+import { Client } from 'minio';
+import { getInput, getBooleanInput, getMultilineInput } from '@actions/core';
+import { lstatSync } from 'fs';
 
-console.log(process.env)
-var endpoint = core.getInput('ENDPOINT')
-var port = core.getInput('PORT')
-var useSSL = core.getBooleanInput('USE-SSL')
+// console.log(process.env)
+var endpoint = getInput('ENDPOINT')
+var port = getInput('PORT')
+var useSSL = getBooleanInput('USE-SSL')
 var accessKey = process.env.MINIO_ACCESS_KEY //core.getInput('ACCESS_KEY')
 var secretKey = process.env.MINIO_ACCESS_SECRET//core.getInput('ACCESS_SECRET')
-var bucket = core.getInput('BUCKET')
+var bucket = getInput('BUCKET')
 
 console.log('check params ', endpoint, port, useSSL, bucket, accessKey, secretKey)
 
-const minioClient = new Minio.Client({
+const minioClient = new Client({
   endPoint: endpoint,
   port: Number.parseInt(port),
   useSSL: useSSL,
@@ -32,7 +32,7 @@ if (exists) {
   console.log('Bucket ' + bucket + ' created in "us-east-1".')
 }
 
-const paths = core.getMultilineInput('paths')
+const paths = getMultilineInput('paths')
 
 if (paths.length === 0) {
   throw ("no paths input")
@@ -43,20 +43,18 @@ var metaData = {
   // 'X-Amz-Meta-Testing': 1234,
   // example: 5678,
 }
-console.log(paths)
+
 paths.map(async (path) => {
-
-
-  try{
-    let stat =  fs.lstatSync(path)
-    console.log('check path => ', path,stat.isDirectory)
+  try {
+    let stat = lstatSync(path)
+    console.log('check path => ', path, stat.isDirectory)
     console.log(stat.metaData)
     if (stat.isDirectory()) {
       throw ('Unsupported')
     } else {
       await minioClient.fPutObject(bucket, path, path, metaData)
     }
-  }catch(e){
+  } catch (e) {
     console.log(e)
   }
 
